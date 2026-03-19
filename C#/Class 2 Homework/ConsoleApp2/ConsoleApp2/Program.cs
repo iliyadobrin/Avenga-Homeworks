8d68c1705bf77c69ace4c4a28ab56f41a66a2ede
iliyadobrin <ilijadobrinovv@gmail.com>
2026-03-19 17:17:32 +0100

    Class 2 Homework

---
+++ C#/Class 2 Homework/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,81 @@
+﻿/*
+ //task1 
+using System;
+
+{
+    static void Main()
+    {
+        Console.Write("Enter the First number: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter the Second number: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter the Operation (+, -, *, /): ");
+        string op = Console.ReadLine();
+
+        double result = 0;
+
+        if (op == "+")
+            result = num1 + num2;
+        else if (op == "-")
+            result = num1 - num2;
+        else if (op == "*")
+            result = num1 * num2;
+        else if (op == "/")
+            result = num1 / num2;
+
+        Console.WriteLine("The result is: " + result);
+    }
+}
+*/
+/*
+//task2
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter the First number: ");
+        double a = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter the Second number: ");
+        double b = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter the Third number: ");
+        double c = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter the Fourth number: ");
+        double d = Convert.ToDouble(Console.ReadLine());
+
+        double average = (a + b + c + d) / 4;
+
+        Console.WriteLine("The average is: " + average);
+    }
+}
+*/
+//task3
+/*
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Input the First Number: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Input the Second Number: ");
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        int temp = a;
+        a = b;
+        b = temp;
+
+        Console.WriteLine("After Swapping:");
+        Console.WriteLine("First Number: " + a);
+        Console.WriteLine("Second Number: " + b);
+    }
+}
+*/
