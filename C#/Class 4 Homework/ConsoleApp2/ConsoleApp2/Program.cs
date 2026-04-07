ccf0e275d7949c76e5aaeb9e362be8bba79aee0b
iliyadobrin <ilijadobrinovv@gmail.com>
2026-04-07 18:10:36 +0200

    Age Calculator

---
+++ C#/Class 4 Homework/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Vnesi go datumot na ragjanje (yyyy-mm-dd): ");
+        if (DateTime.TryParse(Console.ReadLine(), out DateTime birthday))
+        {
+            int age = AgeCalculator(birthday);
+            Console.WriteLine($"Imash {age} godini");
+        }
+        else
+        {
+            Console.WriteLine("Nevaliden format.Koristi yyyy-mm-dd.");
+        }
+    }
+
+    static int AgeCalculator(DateTime birthday)
+    {
+        DateTime today = DateTime.Today;
+        int age = today.Year - birthday.Year;
+
+        if (birthday.Date > today.AddYears(-age))
+            age--;
+
+        return age;
+    }
+}
